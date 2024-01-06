a3f9ef34643af1b01d9ff9e80aeec734bdcb99ed
ivaquero <msaintms@outlook.com>
2024-01-06 12:26:13 +0800

    fix: 🐛 fix a bug in ox-brew

--- ox-brew.sh
+++ ox-brew.sh
@@ -169,7 +169,7 @@ burl() {
 # replace cache file by predownloaded file
 brp() {
     f_pred=$(fd "$1" "${OX_DOWNLOAD}")
-    if [[ -f $f_pred ]]; then
+    if [[ ! -f $f_pred ]]; then
         echo "predownloaded file not found"
         return 1
     fi
