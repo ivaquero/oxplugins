f69c09db5ca3a6652d90d63fcf993ea9019aa577
ivaquero <msaintms@outlook.com>
2024-03-25 23:54:53 +0800

    remove typst funcs in ox-notes

--- ox-notes.sh
+++ ox-notes.sh
@@ -19,21 +19,3 @@ OX_OXIDE[bklgx_]=${OX_BACKUP}/logseq/plugins
 OX_ELEMENT[lg]=${HOME}/.logseq/config/config.edn
 OX_ELEMENT[lgx]=${HOME}/.logseq/config/plugins.edn
 OX_ELEMENT[lgx_]=${HOME}/.logseq/plugins
-
-##########################################################
-# typst
-##########################################################
-
-export TYPST_DATA="${APPDATA}"/typst
-
-# compile typst
-typall() {
-    for file in "${OX_TYPST_ROOT}"/"$1"/*.typ; do typst c "$file"; done
-}
-
-# sync typst lib
-typsync() {
-    for folder in ${OX_TYPST_ENV}; do
-        cp -R -v "${OX_TYPST_LIB}" "$folder"
-    done
-}
